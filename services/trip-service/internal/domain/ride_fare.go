f210b56a3b749be2db3ec40d549a501a81e2d393
Falo <femi.deji0@gmail.com>
2026-05-20 18:31:49 +0100

    Update dependencies in go.mod and go.sum; implement trip service and API gateway functionality with trip preview handling.

---
+++ services/trip-service/internal/domain/ride_fare.go
@@ -0,0 +1,11 @@
+package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type RideFareModel struct {
+	ID                primitive.ObjectID
+	UserID            string
+	PackageSlug       string
+	TotalPriceInCents float64
+	// ExpiresAt time.Time
+}
